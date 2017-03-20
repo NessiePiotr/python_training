cc596d8afddff8096d09a6ee6347b5c545248109
Natalia.Piotrovskaya <Natalia.Piotrovskaya@billing.ru>
2017-03-20 16:03:50 +0300

    Оптимизация кода: в т.ч. использование конструкций if

--- fixture/address.py
+++ fixture/address.py
@@ -6,44 +6,7 @@ class AddressHelper:
     def add_new_element(self, full_name, birthday, company):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
-        # full name
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(full_name.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(full_name.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(full_name.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(full_name.nick_name)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        # company name and address
-        wd.find_element_by_name("company").send_keys(company.company_name)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(company.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(company.home)
-        # phone and e-mail
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(company.phone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(company.e_mail)
-        # birthday
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%d]" % birthday.day).is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[%d]" % birthday.day).click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%d]" % birthday.month).is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[%d]" % birthday.month).click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(birthday.year)
+        self.fill_fields_value(full_name, birthday, company)
         self.app.submit_form()
 
     def delete_first_address(self):
@@ -55,6 +18,11 @@ class AddressHelper:
     def add_edit_element(self, full_name, birthday, company):
         wd = self.app.wd
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        self.fill_fields_value(full_name, birthday, company)
+        wd.find_element_by_name("update").click()
+
+    def fill_fields_value(self, full_name, birthday, company):
+        wd = self.app.wd
         # full name
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -93,5 +61,4 @@ class AddressHelper:
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(birthday.year)
-        wd.find_element_by_name("update").click()
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,4 +6,12 @@ def test_edit_first_group(app):
     app.group.edit_first_group(Group(name="Change First_group", header="Change Start first_group", footer="Change End first_group"))
     app.session.logout()
 
+def test_edit_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="Change First_group"))
+    app.session.logout()
 
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="Change Start first_group"))
+    app.session.logout()
